856d83f7d41ca56d653def381d9e951d2e6d174a
lythanhngodev <lythanhngodev@gmail.com>
2017-12-20 10:45:15 +0700

    ./
    ./

--- admin/ajax/ajax_quen_mat_khau.php
+++ admin/ajax/ajax_quen_mat_khau.php
@@ -2,7 +2,7 @@
 	include_once("../config/config.php");
 	function matkhau() {
 	    $bangchucai = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-	    $matkhauduoctao = array();
+	    $matkhauduoctao = array(); 
 	    $chieudaimang = strlen($bangchucai) - 1;
 	    for ($i = 0; $i < 8; $i++) {
 	        $n = rand(0, $chieudaimang);
